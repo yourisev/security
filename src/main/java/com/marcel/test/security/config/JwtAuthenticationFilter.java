472b19465e09c3674707d372e207c98e4ae25878
yourisev <sevgama44@gmail.com>
2023-09-25 02:04:16 -0400

    Created a JwtAuthenticationFilter class that filters an Http request get the user associated to this request, set the security context associated to this user and chains the responsibility down for the request and response to any Filter concerned. The class SecurityConfiguration is used to configure security measures related to accessing any endpoints on this API. We decide to authorize access to any endpoint under the path "/api/v1/auth/**" and require authentication for all other access request.

---
+++ src/main/java/com/marcel/test/security/config/JwtAuthenticationFilter.java
@@ -0,0 +1,60 @@
+package com.marcel.test.security.config;
+
+import com.marcel.test.security.user.User;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+           @NonNull HttpServletRequest request,
+           @NonNull HttpServletResponse response,
+           @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        //Check JWT Authentication Token
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7); // After Bearer
+        userEmail = jwtService.extractUsername(jwt);// todo extract the userEmail from JWT token;
+        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            User user = (User)this.userDetailsService.loadUserByUsername(userEmail);
+            if(jwtService.isTokenValid(jwt,user)){
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        user,
+                        null,
+                        user.getAuthorities()
+                );
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
+
+    }
+}
